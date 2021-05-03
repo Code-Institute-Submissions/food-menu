747f724df8861f0b7f9d91217de1e33562eef67c
Jonny Björnhager <MiglerSWE@users.noreply.github.com>
2021-05-03 20:04:30 +0200

    Add funciton do show menu and pricing

--- run.py
+++ run.py
@@ -2,7 +2,11 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
-SCOPE = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+SCOPE = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -12,10 +16,47 @@ pizza = SHEET.worksheet('Pizza')
 drinks = SHEET.worksheet('Drinks')
 salads = SHEET.worksheet('Salads')
 
-pizza_data = pizza.get_all_values()
-drinks_data = drinks.get_all_values()
-salads_data = salads.get_all_values()
+pizza_data = pizza.get_all_records()
+drinks_data = drinks.get_all_records()
+salads_data = salads.get_all_records()
+
+def welcome():
+
+    """
+    Prints a welcome message and asks the customer
+    if they want to see the menu. Customer can say yes or no.
+    """
+
+    symbol = '~'
+    print(f'{symbol * 31}\n| Welcome to Italian takeaway |\n{symbol * 31}')
+
+def show_menu():
+    answer = input('Would you like to see the menu? (y/n): ')
+    if answer == 'n':
+        print('Okay! Goodbye!')
+    elif answer == 'y':
+        print_menu(pizza)
+        # print_menu(salads)
+        # print_menu(drinks)
 
 
+def print_menu(sheet):
 
-print(pizza_data)
+    """
+    Prints the menu and the cost of the products
+    """
+    number = 0
+    products = sheet.col_values(1)[1:-1]
+    price = sheet.col_values(2)[1: -1]
+    print()
+    for (item, cost) in zip(products, price):
+        if item == 'margherita':
+            print('PIZZA\n')
+        elif item == 'greek salad':
+            print('SALADS\n')
+        elif item == 'fresh fruit juice':
+            print('DRINKS\n')
+        print(f'{item.capitalize()} ${cost}' )
+
+welcome()
+show_menu()
