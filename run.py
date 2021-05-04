a874f1390b4eba660b11c9da25de53f69261a66b
Jonny Björnhager <MiglerSWE@users.noreply.github.com>
2021-05-04 23:22:38 +0200

    Add function to choose to buy more

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -41,18 +40,24 @@ def main():
     
     
     def show_menu():
-        answer = str(input('Would you like to see the menu? (y/n): '))
+        
+        """
+        Ask the user if they want to see the menu. User has to input either 'y' or
+        'n' to get out of the loop.
+        """
+        
+        answer = ''
+        
+        if answer != 'y' or 'n':
+            answer = input('\nWould you like to see the menu?: ')
 
-        if answer == 'y':
-            chose_product()
-        elif answer == 'n':
-            print('Have a nice day!')
-            exit()
-        elif answer != 'y' or 'n':
-            print('Please type "y" or "n"')
-            show_menu()
-
-
+            if answer == 'y':
+                choose_product()
+            elif answer == 'n':
+                print('Okay. See you next time!')
+                exit()
+                
+            
     
     def show_sub_menu(menu):
         
@@ -65,34 +70,43 @@ def main():
         price = menu.col_values(2)[1: -1]
         for (item, cost) in zip(products, price):
             number += 1
-            print(f'({number}) {item.capitalize()} ${cost}' )
+            print(f'\n({number}) {item.capitalize()} ${cost}' )
 
 
-    def chose_product():
+    def choose_product():
 
         """
         Chose which submenu to buy from. Number has to be between 1 and 3
         """
-
-        print('\nWhat will it be?\n(1)Pizza\n(2)Drink\n(3)Salad\n')
-        answer = int(input('\nNumber: '))
-        if answer == 1:
-            show_sub_menu(pizza)
-            data = get_data(pizza)
-            return data
-            
-        elif answer == 2:
-            show_sub_menu(drinks)
-            data = get_data(drinks)
-            return data
-        elif answer == 3:
-            show_sub_menu(salads)
-            data = get_data(salads)
-            return data
         
+        try:
+            answer = int(input('\nWhat will it be?\n(1)Pizza\n(2)Drink\n(3)Salad\nNumber: '))
+            if answer == 1:
+                show_sub_menu(pizza)
+                data = get_data(pizza)
+                update_sales_worksheet(data)
+
+                
+            elif answer == 2:
+                show_sub_menu(drinks)
+                data = get_data(drinks)
+                update_sales_worksheet(data)
+            elif answer == 3:
+                show_sub_menu(salads)
+                data = get_data(salads)
+                update_sales_worksheet(data)
+        except ValueError as e:
+            print(f'Invalid input: {e}. Please input a number between 1 and 3.')
+            exit()
+            
     
     
     def get_data(menu):
+
+        """
+        Gets the data from the parameter passed
+        into the function.
+        """
         
         products = menu.col_values(1)[1:-1]
         price = menu.col_values(2)[1: -1]
@@ -100,11 +114,12 @@ def main():
         prices = []
         items_cost = 0
         
+        num = int(input("\nWhich one?: "))
+        num -= 1
+
         for cost in price:
             prices.append(cost.replace(',', '.'))
 
-        num = int(input('Which one?: '))
-        num -= 1
                 
         selected_items.append(products[num].capitalize())
                 
@@ -115,28 +130,44 @@ def main():
         return selected_items
     
 
-    welcome()
+   
     
     
-    def update_sales_worksheet():
+    def update_sales_worksheet(data):
         
         """
         Converts the variables into the appropriate types
         and updates the sales sheet with the chosen product
         and the price.
         """
-
-        sales_data = chose_product()
-        converted_price = sales_data[1].replace('.', ',')
-       
+        converted_price = data[1].replace('.', ',')
         final_data = []
-        final_data.append(sales_data[0])
+        final_data.append(data[0])
         final_data.append(converted_price)
         sales.append_row(final_data)
     
-    update_sales_worksheet()
+    welcome()
     
     
+    def buy_more():
+
+        """
+        Asks the user if they want to buy more. If user inputs 'n', the program will exit.
+        """
+
+        answer = ''
+        while answer != 'q':
+            answer = input('\nAnything else?: ')
+
+            if answer == 'y':
+                choose_product()
+            elif answer == 'n':
+                leaving = input('Okay! Press "q" if you want to leave.\n')
+                if leaving == 'q':
+                    print('See you next time!')
+                    exit()
     
+    buy_more()
+
 
 main()
